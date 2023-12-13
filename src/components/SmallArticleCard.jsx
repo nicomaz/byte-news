944efe4f5ec23cd26478ae5afb9fc5c926e3a6fc
nicoMaz <139277771+nicomaz@users.noreply.github.com>
2023-12-13 09:43:10 +0000

    add responsivness until 768px

--- src/components/SmallArticleCard.jsx
+++ src/components/SmallArticleCard.jsx
@@ -7,8 +7,10 @@ const SmallArticleCard = ({ article }) => {
       <img src={article_img_url} alt="" />
       <h3>
         <span className="accent small-text"> {author} </span>
-        <span className="accent-2 small-text">in</span>
-        <span className="accent-3 small-text"> {topic}</span>
+        <span className="accent-3 small-text">
+          <span className="accent-2 small-text">in </span>
+          {topic}
+        </span>
       </h3>
       <h2 className="article-title">{title}</h2>
       <span className="btns small-text">
